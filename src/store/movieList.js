3c4a92c5a066122ea5f42298f6016105b9891318
Steadystudy <sgmin206@naver.com>
2022-05-18 16:27:25 +0900

    환경변수 문제 5

--- src/store/movieList.js
+++ src/store/movieList.js
@@ -67,7 +67,7 @@ const _request = async (options= {}) => {
       params += `&${key}=${value}`
     })
 
-    const res = await fetch(`${process.env.API_END_POINT}?apikey=${process.env.API_KEY}${params}`)
+    const res = await fetch(`${process.env.VUE_APP_API_END_POINT}?apikey=${process.env.VUE_APP_API_KEY}${params}`)
     console.log(res)
     if(res.ok) {
       return await res.json()
