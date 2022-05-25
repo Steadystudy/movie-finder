eaa82e38c1dc1f4c011a412d47976a36f2588413
Steadystudy <sgmin206@naver.com>
2022-05-25 17:32:58 +0900

    refactor: pagination 메서드 정리

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="selectPage">
     <button 
-      v-if="page > 10" 
+      v-if="hasPrevPage()" 
       class="prev" 
-      @click="goPage">이전</button>
+      @click="goPage('이전')">이전</button>
     <template v-for="n in Math.ceil(totalResults/10)" :key="n">
       <div v-if="Math.floor((n-1)/10) === Math.floor((page-1)/10)">
         <button 
           :id="n"
           :class="{active: n === parseInt(page,10)}"
-          @click="goPage">{{n}}</button>
+          @click="goPage(n)">{{n}}</button>
       </div>
     </template>
     <button
-      v-if="totalResults && Math.floor((totalResults-1)/100) !== Math.floor((page-1)/10)"
-      class="next" @click="goPage(this)">다음</button>
+      v-if="hasNextPage()"
+      class="next" @click="goPage('다음')">다음</button>
   </div>
 </template>
 
@@ -26,21 +26,26 @@ export default {
     ...mapState('movieList', ['totalResults','page'])
   },
   methods: {
-    async goPage(event) {
-      let page = this.$store.state.movieList.page
-      const button = event.target.closest('button')
+    async goPage(btn) {
+      let {page} = this.$store.state.movieList
 
-      // 다음 버튼 누를 때 11, 21로 넘어가게
-      if(button.classList.contains('next')) {
+      if(btn === '다음') {
         page = Math.floor((page-1)/10)*10+11
-      // 이전 버튼 누를 때 10, 20로 넘어가게
-      } else if (button.classList.contains('prev')) {
+      } else if (btn === '이전') {
         page = Math.floor((page-1)/10)*10
       } else {
-        page = button.id      
+        page = btn     
       }
 
       await this.$store.dispatch('movieList/changePage', page)
+    },
+    hasNextPage() {
+      let {page, totalResults} = this.$store.state.movieList
+      return totalResults && Math.floor((totalResults-1)/100) !== Math.floor((page-1)/10)
+    },
+    hasPrevPage() {
+      let {page} = this.$store.state.movieList
+      return page > 10
     }
   }
 }
